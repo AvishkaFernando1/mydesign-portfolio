1abd5edf55f949f00096e69fd85b4a3d136e3d7b
AvishkaFernando1 <allroundertech93@gmail.com>
2025-09-17 19:26:53 +0530

    Add files via upload
    new file uplaod

---
+++ send_email.php
@@ -0,0 +1,38 @@
+<?php
+// Check if the request method is POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = strip_tags(trim($_POST["name"]));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $subject = strip_tags(trim($_POST["subject"]));
+    $message = trim($_POST["message"]);
+
+    // Set recipient email (your email)
+    $to = "avishkasfernando2001@gmail.com";
+    
+    // Set email headers
+    $headers = "From: $name <$email>\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+    // Email content
+    $email_content = "<h2>New Contact Form Submission</h2>";
+    $email_content .= "<p><strong>Name:</strong> $name</p>";
+    $email_content .= "<p><strong>Email:</strong> $email</p>";
+    $email_content .= "<p><strong>Subject:</strong> $subject</p>";
+    $email_content .= "<p><strong>Message:</strong><br>" . nl2br($message) . "</p>";
+
+    // Send email
+    if (mail($to, $subject, $email_content, $headers)) {
+        http_response_code(200);
+        echo "Thank You! Your message has been sent.";
+    } else {
+        http_response_code(500);
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+} else {
+    http_response_code(403);
+    echo "There was a problem with your submission, please try again.";
+}
+?>
